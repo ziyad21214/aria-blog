b95c2b109055c644c4530a00b2998e77b49f36c0
ziyad21214 <ziyadsulaiman212@proton.me>
2026-03-08 21:38:19 +0300

    Update routes.py

--- app/blog/routes.py
+++ app/blog/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, session, Response, flash, redirect, url_for 
 from app.models import load_posts
+from app.extensions import limiter
 
 blog = Blueprint('blog', __name__)
 @blog.route('/')
+@limiter.exempt
 def index() -> str:
         posts: list = load_posts()
         posts.sort(key=lambda x: x['date'], reverse=True)
@@ -15,4 +17,5 @@ def show_post(post_id) -> str|Response:
     if post is None:
         flash('Post not found!')
         return redirect(url_for('blog.index'))
+
     return render_template('post.html', post=post, logged_in=session.get('logged_in'))
